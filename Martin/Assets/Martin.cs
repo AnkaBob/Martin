b465b5394a84672c87ab321d1be303cd6831e18d
AnkaBob <jmerlet@ankama.com>
2017-12-14 21:57:21 +0900

    Martin and trapeze

---
+++ Martin/Assets/Martin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Martin : MonoBehaviour {
+
+    private Vector2 _speed = new Vector2(5, 5);
+    private Vector2 _direction;
+    // Use this for initialization
+    void Start () {
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(
+            10,
+            0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void FixedUpdate()
+    {
+    }
+}
---
+++ Martin/Assets/balancing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class balancing : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+
+    void FixedUpdate()
+    {
+        //Component<Rigidbody2D>().rotation += 5;
+    }
+}
