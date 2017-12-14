3575a1e86be11524e6c6579bc9882c6c9b5938e8
AnkaBob <jmerlet@ankama.com>
2017-12-14 23:53:48 +0900

    Martin ne peut plus sauter comme bon lui semble

---
+++ Martin/Assets/scripts/GroundBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Martin")
+        {
+            Martin martin = collider.gameObject.GetComponent<Martin>();
+            martin.EndJump();
+        }
+    }
+}
